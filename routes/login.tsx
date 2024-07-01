9cf571d32135ba7670e227e4ab8f7dee95f63d69
V <veronika.m.winters@gmail.com>
2024-07-01 21:37:35 +0000

    cleanup login route

--- routes/login.tsx
+++ routes/login.tsx
@@ -1,16 +1,8 @@
-import { defineRoute, PageProps } from "$fresh/server.ts";
+import { defineRoute } from "$fresh/server.ts";
 import { getCookies, setCookie } from "jsr:@std/http/cookie";
-import Signin from "../islands/Signin.tsx";
-import { SupabaseClient } from "npm:@supabase/supabase-js";
-import {
-    FreshContext,
-    HandlerContext,
-    RouteContext,
-} from "$fresh/src/server/types.ts";
+import { FreshContext, RouteContext } from "$fresh/src/server/types.ts";
 import { createServerClient } from "npm:@supabase/ssr";
 import Redirect from "../islands/Redirect.tsx";
-import { redirect } from "./lib.ts";
-const kv = await Deno.openKv();
 
 interface Cookie {
     name: string;
@@ -19,9 +11,9 @@ interface Cookie {
 
 const redirect_url = Deno.env.get("REDIRECT_URL") as string;
 export const handler = {
-    async GET(req, ctx: FreshContext) {
+    async GET(req: Request, ctx: FreshContext) {
         let redirect = "";
-        const { searchParams, origin } = new URL(req.url);
+        const { searchParams } = new URL(req.url);
         const code = searchParams.get("code");
 
         const supabase_url = Deno.env.get("SUPABASE_URL") as string;
@@ -52,29 +44,27 @@ export const handler = {
             const { data, error } = await serverClient.auth
                 .exchangeCodeForSession(code);
             console.log({ data, error });
-            const { data: userData, error: userError } = await serverClient
+            const { data: userData, error: _userError } = await serverClient
                 .from("users")
                 .select()
-                .eq("user_id", data.user.id)
+                .eq("user_id", data.user!.id)
                 .single();
             if (!userData) {
                 const { data: userData, error: userError } = await serverClient
                     .from("users")
                     .insert({
-                        user_id: data.user?.id,
-                        username: data.user?.user_metadata.custom_claims
+                        user_id: data.user!.id,
+                        username: data.user!.user_metadata.custom_claims
                             .global_name,
                     });
                 console.log({ userData, userError });
             }
         } else {
-            // const { data, error } = await serverClient.auth.getUser();
-            // console.log({ data, error });
             const val = await serverClient.auth.signInWithOAuth({
                 provider: "discord",
                 options: { redirectTo: redirect_url },
             });
-            redirect = val.data.url;
+            redirect = val.data.url!;
             console.log(val.data.url);
         }
         const resp = await ctx.render({ redirect });
@@ -84,17 +74,18 @@ export const handler = {
     },
 };
 
-export default defineRoute(async (req, ctx) => {
-    const { searchParams, origin } = new URL(req.url);
-    const code = searchParams.get("code");
-    // if "next" is in param, use it as the redirect URL
-    // const cookies = getCookies(req.headers);
+interface Data {
+    redirect: string; // redirect url
+}
 
-    // return redirect(ctx.data.redirect);
+export default defineRoute((req, ctx: RouteContext) => {
+    const { redirect } = ctx.data as Data;
+    const { searchParams } = new URL(req.url);
+    const code = searchParams.get("code");
     return (
         <div>
             {code && <Redirect path="/" />}
-            {!code && <Redirect path={ctx.data.redirect} />}
+            {!code && <Redirect path={redirect} />}
         </div>
     );
 });
