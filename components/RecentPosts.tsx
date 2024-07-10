2a02619b25064cef70dc1e37cf9c7c1a4a67c690
V <veronika.m.winters@gmail.com>
2024-07-10 03:41:17 +0000

    link entire post div

--- components/RecentPosts.tsx
+++ components/RecentPosts.tsx
@@ -13,25 +13,24 @@ interface Data {
 }
 export default function (props: Data) {
     const postsJSX = props.posts.map((post) => (
-        <div class="mb-2">
-            <div>
-                Title:{" "}
-                <a
-                    href={`/communities/${post.communities.name}/${post.id}`}
-                >
-                    {post.title}
-                </a>{" "}
-                {post.category && <span>[{post.category}]</span>}
+        <a
+            href={`/communities/${post.communities.name}/${post.id}`}
+        >
+            <div class="mb-2">
+                <div>
+                    Title: {post.title}{" "}
+                    {post.category && <span>[{post.category}]</span>}
+                </div>
+                <div>Body: {post.body}</div>
+                <div>
+                    Published: {new Date(post.created_at).toLocaleString()}
+                </div>
+                <div>Community: {post.communities.name}</div>
+                <div>
+                    Author: {post.users.username}
+                </div>
             </div>
-            <div>Body: {post.body}</div>
-            <div>
-                Published: {new Date(post.created_at).toLocaleString()}
-            </div>
-            <div>Community: {post.communities.name}</div>
-            <div>
-                Author: {post.users.username}
-            </div>
-        </div>
+        </a>
     ));
     return <div>{postsJSX}</div>;
 }
